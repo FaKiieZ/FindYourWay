169ac22ee8986f774389264431b04bba83fb18b8
Krass Marius <bkrasm@bbc-be.local>
2018-06-04 08:32:24 +0200

    Add ConnectionListAdapter

---
+++ app/src/main/java/ch/bbcag/findyourway/views/ConnectionListAdapter.java
@@ -0,0 +1,22 @@
+package ch.bbcag.findyourway.views;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class ConnectionListAdapter extends ArrayAdapter {
+    public ConnectionListAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
