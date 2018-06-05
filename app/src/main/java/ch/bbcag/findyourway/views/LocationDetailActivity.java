63ca6fbedced68985c24d2509afb2cee718f915a
Remo Rothacher <rothacher.remo1@gmail.com>
2018-06-05 14:21:59 +0200

    fix

--- app/src/main/java/ch/bbcag/findyourway/views/LocationDetailActivity.java
+++ app/src/main/java/ch/bbcag/findyourway/views/LocationDetailActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
